bb97377d0f070a4c6edd45b321c30b07679fd870
valmir-neto <valmir.neto@icen.ufpa.br>
2026-07-05 14:21:11 -0300

    Update Grupo_de_estudo.java

--- src/estrutura/Grupo_de_estudo.java
+++ src/estrutura/Grupo_de_estudo.java
@@ -1,5 +1,81 @@
 package estrutura;
 
-public class Grupo_de_estudo {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Grupo_de_estudo {
+    protected int grupoInt; 
+    protected String nomeGrupo; 
+    protected String pesquisadorResponsavelDiscente; 
+    protected String areaDeEstudo; 
+    private List<Pesquisador> pesquisadores;
+    // Construtor 
+   public Grupo_de_estudo(int grupoInt,String nomeGrupo,String pesquisadorResponsavelDiscente,String areaDeEstudo,List<Pesquisador> pesquisadores) {
+
+    this.grupoInt = grupoInt;
+    this.nomeGrupo = nomeGrupo;
+    this.pesquisadorResponsavelDiscente = pesquisadorResponsavelDiscente;
+    this.areaDeEstudo = areaDeEstudo;
+    this.pesquisadores = (pesquisadores != null)
+            ? pesquisadores
+            : new ArrayList<>();
+}
+
+    //Adicionar e remover pesquisadores do grupo
+   public void adicionarPesquisador(Pesquisador pesquisador) {
+        if (pesquisador != null && !this.pesquisadores.contains(pesquisador)) {
+            this.pesquisadores.add(pesquisador);
+            System.out.println("Pesquisador '" + pesquisador.getNome() + "' adicionado com sucesso ao grupo " + this.nomeGrupo + ".");
+        }
+    }
+        
+    public void removerPesquisador(Pesquisador pesquisador) {
+        if (this.pesquisadores.contains(pesquisador)) {
+            this.pesquisadores.remove(pesquisador);
+            System.out.println("Pesquisador '" + pesquisador.getNome() + "' removido do grupo " + this.nomeGrupo + ".");
+        } else {
+            System.out.println("Este pesquisador não faz parte do grupo " + this.nomeGrupo + ".");
+        }
+    }
+    
+    //Getters e Setters
+    public int getGrupoInt() {
+        return grupoInt;
+    }
+
+    public void setGrupoInt(int grupoInt) {
+        this.grupoInt = grupoInt;
+    }
+
+    public String getNomeGrupo() {
+        return nomeGrupo;
+    }
+
+    public void setNomeGrupo(String nomeGrupo) {
+        this.nomeGrupo = nomeGrupo;
+    }
+
+    public String getPesquisadorResponsavelDiscente() {
+        return pesquisadorResponsavelDiscente;
+    }
+
+    public void setPesquisadorResponsavelDiscente(String pesquisadorResponsavelDiscente) {
+        this.pesquisadorResponsavelDiscente = pesquisadorResponsavelDiscente;
+    }
+
+    public String getAreaDeEstudo() {
+        return areaDeEstudo;
+    }
+
+    public void setAreaDeEstudo(String areaDeEstudo) {
+        this.areaDeEstudo = areaDeEstudo;
+    }
+
+    public List<Pesquisador> getPesquisadores() {
+        return pesquisadores;
+    }
+
+    public void setPesquisadores(List<Pesquisador> pesquisadores) {
+        this.pesquisadores = pesquisadores;
+    }
 }
