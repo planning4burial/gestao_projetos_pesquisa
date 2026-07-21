78717d4d3a2903055e3550ff3a3834966ea8c33b
devNicolas_Pinto <nicolaspinto2003@gmail.com>
2026-07-21 01:41:54 -0300

    Update Relatorio.java
    validação de relatorio

--- src/estrutura/Relatorio.java
+++ src/estrutura/Relatorio.java
@@ -1,71 +1,69 @@
 package estrutura;
+
 import java.util.Date;
 
-public class Relatorio { 
+public class Relatorio {
 	private String titulo;
 	private String conteudo;
 	private Date dataGeracao;
 	private String criador;
-	
-	
-	public Relatorio(String titulo, String conteudo, Date dataGeracao, String criador) {
+	private Boolean valido;
+
+	public Relatorio(String titulo, String conteudo, Date dataGeracao, String criador, Boolean valido) {
 		super();
 		this.titulo = titulo;
 		this.conteudo = conteudo;
 		this.dataGeracao = dataGeracao;
 		this.criador = criador;
+		this.valido = valido;
 	}
-
 
 	public String getTitulo() {
 		return titulo;
 	}
 
-
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
-
 
 	public String getConteudo() {
 		return conteudo;
 	}
 
-
 	public void setConteudo(String conteudo) {
 		this.conteudo = conteudo;
 	}
-
 
 	public Date getDataGeracao() {
 		return dataGeracao;
 	}
 
-
 	public void setDataGeracao(Date dataGeracao) {
 		this.dataGeracao = dataGeracao;
 	}
-
 
 	public String getCriador() {
 		return criador;
 	}
 
-
 	public void setCriador(String criador) {
 		this.criador = criador;
 	}
-	
+
 	public void exibirRelatorio() {
 		System.out.println("=== " + titulo.toUpperCase() + " ===");
-        System.out.println("Gerado por: " + criador);
-        System.out.println("Data: " + dataGeracao);
-        System.out.println("Conteúdo: \n" + conteudo);
-        System.out.println("===============================");
+		System.out.println("Gerado por: " + criador);
+		System.out.println("Data: " + dataGeracao);
+		System.out.println("Conteúdo: \n" + conteudo);
+		System.out.println("===============================");
 	}
 	
+	public Boolean getValido() {
+		return valido;
 	}
 	
+	public void setValido(Boolean valido) {
+		this.valido = valido;
+	}
 
-
-
+}
