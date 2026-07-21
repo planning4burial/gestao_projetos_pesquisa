18ba02920471001a119d9841aca1339cd8305de5
jean souza <741jean@gmail.com>
2026-07-21 02:54:27 -0300

    Validacao de relatorio definida para falso como padrao

--- src/util/Cadastro.java
+++ src/util/Cadastro.java
@@ -10,64 +10,64 @@ import estrutura.Pesquisador;
 import estrutura.Relatorio;
 
 public class Cadastro {
-	
+
 	public static Pesquisador CadastrarPesquisador(Scanner s) {
-		
+
 		System.out.println("Digite seu nome: ");
 		String nome = s.nextLine();
-	
+
 		System.out.println("Digite sua idade: ");
 		int idade = s.nextInt();
-	
+
 		s.nextLine(); // limpando buffer
-	
+
 		System.out.println("Digite seu id institucional: ");
 		String id_institucional = s.nextLine();
-	
+
 		System.out.println("Digite seu email: ");
 		String email = s.nextLine();
-	
+
 		System.out.println("Crie uma senha: ");
 		String senha = s.nextLine();
-	
+
 		return new Pesquisador(nome, email, idade, senha, id_institucional);
-	
+
 	}
 
 	public static Coordenador CadastrarCoordenador(Scanner s) {
-		
+
 		System.out.println("Digite seu nome: ");
 		String nome = s.nextLine();
-	
+
 		System.out.println("Digite sua idade: ");
 		int idade = s.nextInt();
-	
+
 		s.nextLine(); // limpando buffer
-	
+
 		System.out.println("Digite seu id institucional: ");
 		String id_institucional = s.nextLine();
-		
+
 		System.out.println("Digite sua titulacao: ");
 		String titulacao = s.nextLine();
-		
+
 		System.out.println("Digite sua area de atuacao: ");
 		String areaAtuacao = s.nextLine();
-		
+
 		System.out.println("Digite o limite de orientados: ");
 		int limiteOrientados = s.nextInt();
-		
+
 		s.nextLine(); // limpando buffer
-	
+
 		System.out.println("Digite seu email: ");
 		String email = s.nextLine();
-	
+
 		System.out.println("Crie uma senha: ");
 		String senha = s.nextLine();
-	
+
 		return new Coordenador(nome, email, idade, senha, id_institucional, titulacao, areaAtuacao, limiteOrientados);
-	
+
 	}
-	
+
 	public static Orientador CadastrarOrientador(Scanner s) {
 
 		System.out.println("Digite seu nome: ");
@@ -75,7 +75,7 @@ public class Cadastro {
 
 		System.out.println("Digite sua idade: ");
 		int idade = s.nextInt();
-		
+
 		s.nextLine(); // limpando buffer
 
 		System.out.println("Digite seu id institucional: ");
@@ -89,7 +89,7 @@ public class Cadastro {
 
 		System.out.println("Digite seu limite de orientados: ");
 		int limiteOrientados = s.nextInt();
-		
+
 		s.nextLine(); // limpando buffer
 
 		System.out.println("Digite seu SIAPE: ");
@@ -104,7 +104,9 @@ public class Cadastro {
 		System.out.println("Crie uma senha: ");
 		String senha = s.nextLine();
 
-		return new Orientador(nome, idade, email, senha, id_institucional, titulacao, areaAtuacao, limiteOrientados, siape, institutoVinculado);
+		return new Orientador(nome, idade, email, senha, id_institucional, titulacao, areaAtuacao, limiteOrientados,
+				siape, institutoVinculado);
+
 	}
 
 	public static Gestor CadastrarGestor(Scanner s) {
@@ -114,7 +116,7 @@ public class Cadastro {
 
 		System.out.println("Digite sua idade: ");
 		int idade = s.nextInt();
-		
+
 		s.nextLine(); // limpando buffer
 
 		System.out.println("Digite seu id institucional: ");
@@ -128,7 +130,7 @@ public class Cadastro {
 
 		System.out.println("Digite seu limite de orientados: ");
 		int limiteOrientados = s.nextInt();
-		
+
 		s.nextLine(); // limpando buffer
 
 		System.out.println("Digite seu SIAPE: ");
@@ -143,24 +145,24 @@ public class Cadastro {
 		System.out.println("Crie uma senha: ");
 		String senha = s.nextLine();
 
-		return new Gestor(nome, idade, email, senha, id_institucional, titulacao, areaAtuacao, limiteOrientados, siape, institutoVinculado);
+		return new Gestor(nome, idade, email, senha, id_institucional, titulacao, areaAtuacao, limiteOrientados, siape,
+				institutoVinculado);
+
 	}
-	
+
 	public static Relatorio CadastrarRelatorio(Scanner s) {
-		
+
 		System.out.println("Digite o titulo do relatorio: ");
 		String titulo = s.nextLine();
-		
+
 		System.out.println("Digite o conteudo do relatorio:\n");
 		String conteudo = s.nextLine();
-		
+
 		Date dataGeracao = new Date();
-		
+
 		System.out.println("Digite seu nome: ");
 		String criador = s.nextLine();
-		
-		Boolean valido = false;
-		
-		return new Relatorio(titulo, conteudo, dataGeracao, criador, valido);
+
+		return new Relatorio(titulo, conteudo, dataGeracao, criador, false);
 	}
 }
