c165a9247cac55399461a2eea0848d8597dd7e44
jean souza <741jean@gmail.com>
2026-07-20 23:45:28 -0300

    testando classe cadastro

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import estrutura.Pesquisador;
+import util.Cadastro;
 
 public class Main {
 	public static void main(String[]args) {
@@ -16,28 +17,9 @@ public class Main {
 		//so deve criar objeto apos usuario selecionar menu pesquisador -> cadastrar
 		
 		//String nome, String email, int idade, String senha, String idInstitucional
-		Scanner scanner = new Scanner(System.in);
+		Scanner s = new Scanner(System.in);
 		
-		System.out.println("Digite seu nome: ");
-		String nome = scanner.nextLine();
-		
-		System.out.println("Digite sua idade: ");
-		int idade = scanner.nextInt();
-		
-		scanner.nextLine(); // limpando buffer
-		
-		System.out.println("Digite seu id institucional: ");
-		String id_institucional = scanner.nextLine();
-		
-		System.out.println("Digite seu email: ");
-		String email = scanner.nextLine();
-		
-		System.out.println("Crie uma senha: ");
-		String senha = scanner.nextLine();
-		
-		
-		Pesquisador p = new Pesquisador(nome, email, idade, senha, id_institucional);
-		pesquisadores.add(p);
+		pesquisadores.add(Cadastro.CadastrarPesquisador(s));
 		
 	}
 }
